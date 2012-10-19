20bdb7b916292328eb6709f1511e760bf9526b2b
unknown <382316385@qq.com>
2012-10-19 11:23:55 +0800

    -Handle Exception

--- proxy.py
+++ proxy.py
@@ -17,13 +17,19 @@ agent = Agent(reactor)
 #处理用户的请求
 class MyProxyRequest(proxy.ProxyRequest):
     def process(self):
-        logger.debug(self.getAllHeaders())
-        logger.debug('uri: %s method: %s'%(self.uri,self.method))
-        logger.debug('request headers: %s'%self.requestHeaders)
+        #logger.debug(self.getAllHeaders())
+        logger.debug('uri: %s \nmethod: %s'%(self.uri,self.method))
+        #logger.debug('request headers: %s'%self.requestHeaders)
         self.defer = agent.request(self.method, self.uri, headers=self.requestHeaders)
         self.defer.addCallback(handle,request=self)
         self.defer.addErrback(handleErr,request=self)
-    
+        
+    def connectionLost(self, reason):
+        if self is not None:
+            logger.debug('Remote Host have no response \n %s\n'%reason)
+            self.finish()
+            #self = None
+   
 class MyProxy(proxy.Proxy):
     requestFactory = MyProxyRequest
     
@@ -38,29 +44,34 @@ class MyProxyResponse(http.HTTPClient):
         
     def connectionLost(self, reason):
         if isinstance(reason.value, client.ResponseDone):
-            self.request.finish()
-        else: print reason
+            if self.request is not None:
+                logger.debug('ResponseDone')
+                self.request.finish()
+                self.request = None
+        else: print 'Some other Error'
+        
     def dataReceived(self, data):
         self.request.write(data)
             
 
 #处理从网页下载下来的回应
 def handle(response,request):
-    logger.debug('response.code%d'%response.code)
+    #logger.debug('response.code%d'%response.code)
     request.setResponseCode(response.code)
     request.responseHeaders = response.headers
     if response.code in (304, 404):
-            request.notifiyFinish().addCallback(closeRequest,request)
+            request.finish()
+            request = None
     else: 
         response.deliverBody(MyProxyResponse(request,response))
 
 #处理请求失败的情况        
 def handleErr(reason,request):
-    print 'Error'
-    request.notifiyFinish().addCallback(closeRequest,request)
-
-def closeRequest(r):
-    r.finish()
+    #print 'handleErr%s: '%reason
+    if request is not None:
+        request.finish()
+        request = None
+    
 if __name__ == '__main__':
     reactor.listenTCP(ServerPort,MyProxyFactory())
     logger.debug('server is listening at %d'%ServerPort)
