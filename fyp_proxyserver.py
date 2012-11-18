cfcb4bfa7bb039f08c27e78d0a6f276a3049ae44
yujie <382316385@qq.com>
2012-11-18 21:34:15 +0800

    support https

--- fyp_proxyserver.py
+++ fyp_proxyserver.py
@@ -1,4 +1,10 @@
-from twisted.internet import protocol,reactor,endpoints
+#coding:utf-8
+# Author:  yujie
+# Purpose: FYP
+# Created: 2012/11/18
+
+
+from twisted.internet import protocol,reactor,endpoints,ssl
 import re,struct,socks5,socket
 
 class ProxyFactory(protocol.Factory):
@@ -24,15 +30,16 @@ class ProxyServerProtocol(protocol.Protocol):
     TYPES = ('HTTP','SOCK5')
     proxyType = ''
     times = 0
-    HTTP_STATUS_CODE=(304,404)
     sock5Header = None
     
     def dataReceived(self, data):        
         self.times += 1
         if self.times is 1:
             self.proxyType = self.TYPES[0] if len(data) >30 else self.TYPES[1]
+            
         if self.proxyType == 'HTTP':
             self.__handleHttpRequest(data)
+            
         elif self.proxyType == 'SOCK5':
             self.sock5Header = socks5.Sock5Header()
             self.__handleSock5Auth(data)
@@ -49,10 +56,10 @@ class ProxyServerProtocol(protocol.Protocol):
         if headers.has_key('Host'):
             client = endpoints.TCP4ClientEndpoint(reactor, headers['Host'], 80)
             client.connect(ProxyFactory(ProxyHttpClientProtocol,
-                                        oppsite=self,data=data)).addErrback(handleErr,self)
-    
+                                       oppsite=self,data=data)).addErrback(handleHttpErr,self)
+        else: self.transport.loseConnection()
+        
     def __handleSock5Auth(self,data):
-        #print repr(data)
         if self.times == 1:
             self.transport.write(self.sock5Header.NO_AUTHENTICATION_REQUIRED())
             
@@ -60,12 +67,10 @@ class ProxyServerProtocol(protocol.Protocol):
             header_length = len(data)
             host = data[5:header_length-2]
             port =struct.unpack('>H',data[header_length-2:header_length])[0] 
-            #print host,"---------------",port
             client = endpoints.TCP4ClientEndpoint(reactor, host, port)
             client.connect(ProxyFactory(ProxySock5ClientProtocol, 
-                                    oppsite=self,data=self.sock5Header)).addErrback(handleErr,self)
-        elif self.times == 3:
-            #print self.oppsite
+                                oppsite=self,data=self.sock5Header)).addErrback(handleSock5Err,self)
+        elif self.times >= 3:
             self.oppsite.transport.write(data)
             
 class ProxyHttpClientProtocol(protocol.Protocol):
@@ -77,30 +82,37 @@ class ProxyHttpClientProtocol(protocol.Protocol):
         self.oppsite.transport.write(data)
         
     def connectionLost(self, reason):        
-        self.oppsite.transport.loseConnection()
         self.transport.loseConnection()
 
 class ProxySock5ClientProtocol(protocol.Protocol):
             
     def connectionMade(self):
         curhost = self.transport.getHost()
-        addr, port = curhost.host,curhost.port        
+        addr, port = curhost.host,curhost.port
+        #('192.168.162.95', 5204)   '\x05\x00\x00\x01\xc0\xa8\xa2_\x14T'
+        #print 'Reply:',repr(self.factory.header.RM_REPLY_SUCCESS(addr='192.168.162.95',port=5204))
         self.oppsite.transport.write(self.factory.header.RM_REPLY_SUCCESS(addr=addr,port=port))
         
     def dataReceived(self, data):
         self.oppsite.transport.write(data)
         
     def connectionLost(self, reason):        
-        self.oppsite.transport.loseConnection() 
-        self.transport.loseConnection()
+        self.transport.loseConnection() 
         
-def handleErr(reason,protocol):
-    print protocol.factory.addr,'refuse'
-    protocol.transport.loseConnection()  
-
+def handleHttpErr(reason,protocol):
+    print 'Server refuse Http'
+    protocol.transport.loseConnection() 
+    
+def handleSock5Err(reason,protocol):
+    print 'Server refuse Sock5'
+    #protocol.transport.write()
+    protocol.transport.loseConnection()
+    
 if __name__ == '__main__':    
-    e = endpoints.TCP4ServerEndpoint(reactor, 1080)
+    
+    PORT = 9000
+    e = endpoints.TCP4ServerEndpoint(reactor, PORT)
     f = ProxyFactory(ProxyServerProtocol)
     e.listen(f)
-    print 'Server is running at 1080'
+    print 'Server is running at', PORT
     reactor.run()
